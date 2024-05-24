7147994f2df3176bd63bc9809c9e48a4ceca0a8f
NicolasMatte <nicolas.matte@etu.minesparis.psl.eu>
2024-05-24 19:53:36 +0200

    app_first_draft

---
+++ Grupo_3/app/app/routes.py
@@ -0,0 +1,25 @@
+from flask import render_template
+from flask_session import Session
+from app import app
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import sys
+
+matplotlib.use('Agg') # Non interactive backend mode
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
+
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+
+# INDEX
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html',title='Home')
+ 
+
---
+++ Grupo_3/app/readme.txt
@@ -0,0 +1,9 @@
+In a Git Bash terminal :
+
+$ export FLASK_APP=run.py
+
+$ flask run
+
+The application will be available on your browser at :
+
+http://localhost:5000/
