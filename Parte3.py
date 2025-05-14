0e97409f1a54a4c0439ed005e12fbe4205f78980
Jinela <jinela.gonzalez@alumnos.upm.es>
2025-05-14 22:12:13 +0200

    codigo de data diaria

--- Parte3.py
+++ Parte3.py
@@ -1,6 +1,6 @@
 import requests
 import pandas as pd
-
+#Definimos la API_KEY y la URL base
 API_KEY    = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqaW5lbGEuZ29uemFsZXpAYWx1bW5vcy51cG0uZXMiLCJqdGkiOiJmZjU4ZTJlNi1iMjVhLTQ1ZTAtYTUzYi0xZDBmNDY3OGJhZDgiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTc0NjgyMjkxNywidXNlcklkIjoiZmY1OGUyZTYtYjI1YS00NWUwLWE1M2ItMWQwZjQ2NzhiYWQ4Iiwicm9sZSI6IiJ9.Cy_fCJ8NZSgQHadQEOoH-feniDOlu6CgaJ1ZBFX4y5c"
 BASE_URL   = "https://opendata.aemet.es/opendata/api"
 # Definimos las funciones para obtener los datos de la API
