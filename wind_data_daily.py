c25a43ecee49b15983491e982a6adc5c218fbe48
Jinela <jinela.gonzalez@alumnos.upm.es>
2025-05-16 13:17:12 +0200

    act en comentarios

--- wind_data_daily.py
+++ wind_data_daily.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 
-#Clave API para AEMET
+#Clave API para ingresr a AEMET
 # https://opendata.aemet.es/centrodedescargas/inicio
 API_KEY    = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqaW5lbGEuZ29uemFsZXpAYWx1bW5vcy51cG0uZXMiLCJqdGkiOiJmZjU4ZTJlNi1iMjVhLTQ1ZTAtYTUzYi0xZDBmNDY3OGJhZDgiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTc0NjgyMjkxNywidXNlcklkIjoiZmY1OGUyZTYtYjI1YS00NWUwLWE1M2ItMWQwZjQ2NzhiYWQ4Iiwicm9sZSI6IiJ9.Cy_fCJ8NZSgQHadQEOoH-feniDOlu6CgaJ1ZBFX4y5c"
 BASE_URL   = "https://opendata.aemet.es/opendata/api"
