2ddc4c5a6912c8f402dfedf26780a026c2009998
jfaure10 <javier.faure@alumnos.upm.es>
2025-05-29 14:06:19 +0200

    Delete Parte3.py

--- Parte3.py
+++
@@ -1,9 +0,0 @@
-import requests
-import pandas as pd
-# Clave para AEMET .
-API_KEY    = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqaW5lbGEuZ29uemFsZXpAYWx1bW5vcy51cG0uZXMiLCJqdGkiOiJmZjU4ZTJlNi1iMjVhLTQ1ZTAtYTUzYi0xZDBmNDY3OGJhZDgiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTc0NjgyMjkxNywidXNlcklkIjoiZmY1OGUyZTYtYjI1YS00NWUwLWE1M2ItMWQwZjQ2NzhiYWQ4Iiwicm9sZSI6IiJ9.Cy_fCJ8NZSgQHadQEOoH-feniDOlu6CgaJ1ZBFX4y5c"
-BASE_URL   = "https://opendata.aemet.es/opendata/api"
-
-print("Mandar codigo nuevo al main")
-
-
