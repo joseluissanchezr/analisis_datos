598eb68037f62e67a9d46b517a7f04c83b4090e1
Killian <killian.bujon@etu.minesparis.psl.eu>
2026-05-25 11:11:40 +0200

    Killian's code added to the notebook

--- Grupo1_2026/API.py
+++ Grupo1_2026/API.py
@@ -2,7 +2,7 @@ from entsoe import EntsoePandasClient
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 #DL the module entsoe 
 
 client = EntsoePandasClient(
